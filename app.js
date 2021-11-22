e64505b1fb9c24a198da5f221fbad435c4f2bcdf
Frazonar <snip.coloured@gmail.com>
2021-11-22 16:02:11 +0200

    feat: implement cards routing

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const users = require('./routes/users');
+const cards = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.use('/users', users);
+app.use('/cards', cards);
 
 app.listen(PORT, () => {
   //
--- routes/cards.js
+++ routes/cards.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
 
 router.get('/', (req, res) => {
-  //
+  const cardDataPath = path.join(__dirname, '../data/cards.json');
+  const reader = fs.createReadStream(cardDataPath, { encoding: 'utf8' });
+  let cardData = '';
+
+  reader.on('data', (chunk) => {
+    cardData += chunk;
+  });
+
+  reader.on('end', () => {
+    const parsedCardData = JSON.parse(cardData);
+    res.send(parsedCardData);
+  });
+
+  reader.on('error', () => {
+    res.status(500).send({ message: 'An error has occured on the server' });
+  });
 });
 
 module.exports = router;
