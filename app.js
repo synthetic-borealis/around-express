0328d0d30f818974dbcde36bfccaaa826467e6d9
Frazonar <snip.coloured@gmail.com>
2021-11-28 21:46:14 +0200

    connect app.js to the database

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
+const mongoose = require('mongoose');
+
 const users = require('./routes/users');
 const cards = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/aroundb');
 
 const sendNotFoundMessage = (req, res, next) => {
   res.status(404).send({ message: 'Requested resource not found' });
