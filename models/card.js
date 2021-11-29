56348c50d9d78cc8a5a08350a3c24c50a5a01042
Frazonar <snip.coloured@gmail.com>
2021-11-29 10:17:06 +0200

    feat: create card model

---
+++ models/card.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const { urlRegex } = require('../utils/helpers');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return v.match(urlRegex);
+      },
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
--- models/user.js
+++ models/user.js
@@ -25,4 +25,4 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-module.exports = userSchema;
+module.exports = mongoose.model('user', userSchema);
